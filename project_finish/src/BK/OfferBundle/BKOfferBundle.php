229e3248b760d8323e6439506c0c79b09264baa0
Błażej Kostrzewa <kostrzewa.blazej@gmail.com>
2017-05-12 06:03:33 +0200

    first commit-  logic without bootstrap

---
+++ project_finish/src/BK/OfferBundle/BKOfferBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BK\OfferBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BKOfferBundle extends Bundle
+{
+}
---
+++ project_finish/src/BK/OfferBundle/Form/OfferType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BK\OfferBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class OfferType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('title')->add('description')->add('price')->add('dateEnd');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BK\OfferBundle\Entity\Offer'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'bk_offerbundle_offer';
+    }
+
+
+}
